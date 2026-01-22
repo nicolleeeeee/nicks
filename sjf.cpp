b7e24764f201a874833115fb2419b84a70de8d9f
nicolleeeeee <roconicole98@gmail.com>
2026-01-22 21:38:20 +0000

    sjf

---
+++ sjf.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+        int bt[10], wt[10], tat[10];
+
+            cout << "Enter number of processes: ";
+                cin >> n;
+
+                    cout << "Enter burst time:\n";
+                        for(int i=0;i<n;i++)
+                                cin >> bt[i];
+
+                                    for(int i=0;i<n-1;i++)
+                                            for(int j=i+1;j<n;j++)
+                                                        if(bt[i] > bt[j])
+                                                                        swap(bt[i], bt[j]);
+
+                                                                            wt[0] = 0;
+                                                                                for(int i=1;i<n;i++)
+                                                                                        wt[i] = wt[i-1] + bt[i-1];
+
+                                                                                            cout << "\nP\tBT\tWT\tTAT\n";
+                                                                                                for(int i=0;i<n;i++) {
+                                                                                                        tat[i] = bt[i] + wt[i];
+                                                                                                                cout << "P" << i+1 << "\t" << bt[i] << "\t" << wt[i] << "\t" << tat[i] << endl;
+                                                                                                                    }
+                                                                                                                    }
